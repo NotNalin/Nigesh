08ab49c12fa4c96f9ccc20c7a10e6d10578f892d
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-06-07 16:04:42 +0400

    reduced lines

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def ping(ctx):
 
 @client.slash_command(name="ping", description="Returns the bot's latency")
 async def ping(ctx):
-    await ctx.respond(f'{round(client.latency * 1000)}ms')
+    await ctx.respond(f'{round(client.latency * 1000)}ms', ephemeral=True)
 
 
 @client.command(aliases=["bal"])
@@ -36,18 +36,12 @@ async def balance(ctx, card):
 async def nol(ctx, card):
     nol_details = Nol.Details(card)
     if nol_details['Error'] is False:
-        embed = discord.Embed(title=f"NOL Details",
-                              description=f"{nol_details['NolID']}")
-        embed.set_thumbnail(
-            url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
-        embed.add_field(name="Card Balance",
-                        value=f"{nol_details['Card Balance']}")
-        embed.add_field(name="Pending Balance",
-                        value=f"{nol_details['Pending Balance']}")
-        embed.add_field(name="Expiry Date",
-                        value=f"{nol_details['Expiry Date']}")
-        embed.set_footer(
-            text=f"Please note that the shown balance may not include transactions occurred in the past 48 hours")
+        embed = discord.Embed(title=f"NOL Details", description=f"{nol_details['NolID']}")
+        embed.set_thumbnail(url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
+        embed.add_field(name="Card Balance", value=f"{nol_details['Card Balance']}")
+        embed.add_field(name="Pending Balance", value=f"{nol_details['Pending Balance']}")
+        embed.add_field(name="Expiry Date", value=f"{nol_details['Expiry Date']}")
+        embed.set_footer(text=f"Please note that the shown balance may not include transactions occurred in the past 48 hours")
         await ctx.respond(embed=embed, ephemeral=True)
     else:
         await ctx.respond(nol_details['ErrorMsg'], ephemeral=True)
@@ -57,18 +51,12 @@ async def nol(ctx, card):
 async def nol(ctx, card):
     nol_details = Nol.Details(card)
     if nol_details['Error'] is False:
-        embed = discord.Embed(title=f"NOL Details",
-                              description=f"{nol_details['NolID']}")
-        embed.set_thumbnail(
-            url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
-        embed.add_field(name="Card Balance",
-                        value=f"{nol_details['Card Balance']}")
-        embed.add_field(name="Pending Balance",
-                        value=f"{nol_details['Pending Balance']}")
-        embed.add_field(name="Expiry Date",
-                        value=f"{nol_details['Expiry Date']}")
-        embed.set_footer(
-            text=f"Please note that the shown balance may not include transactions occurred in the past 48 hours")
+        embed = discord.Embed(title=f"NOL Details", description=f"{nol_details['NolID']}")
+        embed.set_thumbnail(url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
+        embed.add_field(name="Card Balance", value=f"{nol_details['Card Balance']}")
+        embed.add_field(name="Pending Balance", value=f"{nol_details['Pending Balance']}")
+        embed.add_field(name="Expiry Date", value=f"{nol_details['Expiry Date']}")
+        embed.set_footer(text=f"Please note that the shown balance may not include transactions occurred in the past 48 hours")
         await ctx.reply(embed=embed)
     else:
         await ctx.reply(nol_details['ErrorMsg'])
@@ -79,19 +67,15 @@ async def recent(ctx, card, transaction=1):
     recent = Nol.Recent(card, transaction)
     if recent['Error'] is False:
         if transaction == 1:
-            embed = discord.Embed(
-                title=f"Last NOL Transaction", description=f"{recent['NolID']}")
+            embed = discord.Embed(title=f"Last NOL Transaction", description=f"{recent['NolID']}")
         else:
-            embed = discord.Embed(
-                title=f"Recent NOL Transaction Number {transaction}", description=f"{recent['NolID']}")
-        embed.set_thumbnail(
-            url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
+            embed = discord.Embed(title=f"Recent NOL Transaction Number {transaction}", description=f"{recent['NolID']}")
+        embed.set_thumbnail(url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
         embed.add_field(name="Date", value=f"{recent['Date']}")
         embed.add_field(name="Time", value=f"{recent['Time']}")
         embed.add_field(name="Transaction Type", value=f"{recent['Type']}")
         embed.add_field(name="Amount", value=f"{recent['Amount']} AED")
-        embed.set_footer(
-            text=f"Please note that the bot only shows transactions occurred in the past month")
+        embed.set_footer(text=f"Please note that the bot only shows transactions occurred in the past month")
         await ctx.reply(embed=embed)
     else:
         await ctx.reply(recent['ErrorMsg'])
@@ -101,30 +85,28 @@ async def recent(ctx, card, transaction=1):
 async def bal(ctx, card: discord.Option(str, "NOL Card Number", requied=True)):
     nolbal = Nol.Details(card)
     if nolbal['Error'] is False:
-        await ctx.respond(f"Your Nol card balance is : {nolbal['Balance']}", ephemeral=True)
+        await ctx.respond(f"Your Nol card balance is : {nolbal['Balance']}")
     else:
-        await ctx.respond(f"{card} is not a valid NOL Card", ephemeral=True)
+        await ctx.respond(f"{card} is not a valid NOL Card")
+
 
 @client.slash_command(name="recent", description="Check your NOL Card recent transactions")
 async def recent(ctx, card: discord.Option(str, "NOL Card Number", requied=True), transaction: discord.Option(int, description="Number of recent transaction to show", min_value=1, default=1)):
     recent = Nol.Recent(card, transaction)
     if recent['Error'] is False:
         if transaction == 1:
-            embed = discord.Embed(
-                title=f"Last NOL Transaction", description=f"{recent['NolID']}")
+            embed = discord.Embed(title=f"Last NOL Transaction", description=f"{recent['NolID']}")
         else:
-            embed = discord.Embed(
-                title=f"Recent NOL Transaction Number {transaction}", description=f"{recent['NolID']}")
-        embed.set_thumbnail(
-            url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
+            embed = discord.Embed(title=f"Recent NOL Transaction Number {transaction}", description=f"{recent['NolID']}")
+        embed.set_thumbnail(url="https://www.rta.ae/wps/wcm/connect/rta/3ae021ee-ea75-4c10-a579-35ab58bcf20d/apps.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_N004G041LOBR60AUHP2NT32000-3ae021ee-ea75-4c10-a579-35ab58bcf20d-nUKFITN")
         embed.add_field(name="Date", value=f"{recent['Date']}")
         embed.add_field(name="Time", value=f"{recent['Time']}")
         embed.add_field(name="Transaction Type", value=f"{recent['Type']}")
         embed.add_field(name="Amount", value=f"{recent['Amount']} AED")
         embed.set_footer(text=f"Please note that the bot only shows transactions occurred in the past month")
-        await ctx.respond(embed=embed, ephemeral=True)
+        await ctx.respond(embed=embed)
     else:
-        await ctx.respond(recent['ErrorMsg'], ephemeral=True)
+        await ctx.respond(recent['ErrorMsg'])
 
 
 client.run(os.environ['TOKEN'])
